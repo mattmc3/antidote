a4c6d1742e30157ee64ad80e2549bd1d6807b999
mattmc3 <mattmc3@gmail.com>
2021-01-14 13:04:59 -0500

    Add tests for `pz zcompile -c`

--- tests/zcompile.zsh
+++ tests/zcompile.zsh
@@ -20,4 +20,13 @@ pz zcompile > /dev/null 2>&1
 @test "fakemyzsh/themes/russellbobby is compiled" -f $PZ_PLUGIN_HOME/fakemyzsh/themes/russellbobby.zsh-theme.zwc
 @test "preztno/modules/soarin is compiled" -f $PZ_PLUGIN_HOME/preztno/modules/soarin/init.zsh.zwc
 
+# zcompile clean
+pz zcompile -c > /dev/null 2>&1
+@test 'pz zcompile -c (clean) succeeds' $? -eq 0
+
+@test "zsh-incompletions.plugin.zsh zwc was cleaned" ! -f $PZ_PLUGIN_HOME/zsh-incompletions/zsh-incompletions.plugin.zsh.zwc
+@test "fakemyzsh/lib/one zwc was cleaned" ! -f $PZ_PLUGIN_HOME/fakemyzsh/lib/one.zsh.zwc
+@test "fakemyzsh/themes/russellbobby zwc was cleaned" ! -f $PZ_PLUGIN_HOME/fakemyzsh/themes/russellbobby.zsh-theme.zwc
+@test "preztno/modules/soarin zwc was cleaned" ! -f $PZ_PLUGIN_HOME/preztno/modules/soarin/init.zsh.zwc
+
 teardown
