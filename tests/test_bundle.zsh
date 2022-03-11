56df37eb58d9bd9a9d6bf6fe51391fbbf8639686
mattmc3 <mattmc3@gmail.com>
2022-03-11 13:13:41 -0500

    Add bundler tests and refactor others

--- tests/test_bundle.zsh
+++ tests/test_bundle.zsh
@@ -2,8 +2,8 @@
 @echo "=== ${0:t:r} ==="
 
 autoload -Uz ${0:a:h}/functions/setup && setup
-
 source $PRJ_HOME/antidote.zsh
+
 ZSH_PLUGINS_TXT=${0:a:h}/misc/zsh_plugins.txt
 ZSH_PLUGINS_ZSH=${ZSH_PLUGINS_TXT:r}.zsh
 
@@ -14,27 +14,29 @@ function _antidote_gitclone { _mock_gitclone "$@" }
 actual_repos=($ANTIDOTE_HOME/*(N/))
 @test "nothing has been cloned" $#actual_repos -eq 0
 
-# either write @echo fd3 to /dev/null or to &1
-3>&1 antidote bundle <$ZSH_PLUGINS_TXT >| $PRJ_HOME/.cache/bundle_actual.txt
+# we need to redirect @echo fd3 output to somewhere
+# logs, /dev/null, &1...
+3>$ZTAP_LOG_HOME/${0:t:r}.git.log antidote bundle <$ZSH_PLUGINS_TXT >/dev/null
 @test "antidote bundle succeeds" $? -eq 0
 
 actual_repos=($ANTIDOTE_HOME/*(N/))
 expected_repos=($TEST_HOME/fakerepos/*(N/))
 @test "all repos have been cloned" $#actual_repos -eq $#expected_repos
 
-actual=("${(@f)$(antidote bundle <$ZSH_PLUGINS_TXT)}")
-expected=("${(f)"$(<$ZSH_PLUGINS_ZSH)"}")
+STATICFILE=$ZTAP_LOG_HOME/${0:t:r}.actual.log
+3>$ZTAP_LOG_HOME/${0:t:r}_2.git.log antidote bundle <$ZSH_PLUGINS_TXT >$STATICFILE
+
+actual=("${(f)$(<$STATICFILE)}")
+expected=("${(f)$(<$ZSH_PLUGINS_ZSH)}")
 expected=(${expected//\$ANTIDOTE_HOME/$ANTIDOTE_HOME})
 
 @test "antidote bundle produces the expected output line count" $#expected -eq $#actual
 
-# debuging help
+# debuging help - let's not spam the output with the gory details
 if [[ "$expected" = "$actual" ]]; then
   @test "antidote bundle produces the expected output" 1 -eq 1
 else
-  @test "antidote bundle produces the expected output" "see bundle_expected.txt" = "see bundle_actual.txt"
-  printf "%s\n" "${expected[@]}" >| $PRJ_HOME/.cache/bundle_expected.txt
-  printf "%s\n" "${actual[@]}" >| $PRJ_HOME/.cache/bundle_actual.txt
+  @test "antidote bundle produces the expected output" "compare $ZSH_PLUGINS_TXT" = "to $STATICFILE"
 fi
 
 teardown
---
+++ tests/test_bundler.zsh
@@ -0,0 +1,26 @@
+0=${(%):-%x}
+@echo "=== ${0:t:r} ==="
+
+autoload -Uz ${0:a:h}/functions/setup && setup
+source $PRJ_HOME/antidote.zsh
+
+# mocks
+# comment this out to test actually cloning repos, but unless you somehow think git
+# or the git command is broken it's probably not necessary
+function _antidote_gitclone { _mock_gitclone "$@" }
+
+BUNDLEFILE=$TEMP_HOME/.zsh_plugins.txt
+STATICFILE="${BUNDLEFILE:r}.zsh"
+cp "${0:a:h}/misc/zsh_plugins.txt" "$BUNDLEFILE"
+
+@test "no static file exists" ! -f "$STATICFILE"
+
+# we need to redirect @echo fd3 output to somewhere
+# logs, /dev/null, &1...
+3>$ZTAP_LOG_HOME/${0:t:r}.git.log antidote bundler "$BUNDLEFILE"
+@test "antidote bundler succeeds" $? -eq 0
+
+@test "a static file now exists" -f "$STATICFILE"
+
+@echo "$ZTAP_LOGS"
+teardown
