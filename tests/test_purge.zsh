0a7913a36832effa7d6cdd44874d157b554fc795
mattmc3 <mattmc3@gmail.com>
2022-09-02 10:00:55 -0400

    Cleanup purge and add tests

--- tests/test_purge.zsh
+++ tests/test_purge.zsh
@@ -7,8 +7,8 @@ ztap_header "${0:t:r}"
 ANTIDOTE_HOME=$BASEDIR/tests/fakezdotdir/antidote_home
 source $BASEDIR/antidote.zsh
 
+# purge missing arg
 () {
-  # test with no arg
   local actual expected exitcode
   expected="antidote: error: required argument 'bundle' not provided, try --help"
   actual=$(antidote purge 2>&1)
@@ -17,6 +17,7 @@ source $BASEDIR/antidote.zsh
   @test "'antidote purge' with no args fail message" "$expected" = "$actual"
 }
 
+# purge missing bundle
 () {
   # test with repo arg but repo does not exist
   local actual expected exitcode bundle bundledir
@@ -30,27 +31,30 @@ source $BASEDIR/antidote.zsh
   @test "'antidote purge' missing bundle fail message" "$expected" = "$actual"
 }
 
+# antidote purge removes dir and comments out bundle
 () {
-  local actual expected exitcode bundle bundledir
+  local actual exitcode bundle bundledir
+  local pluginsfile expectedfile diffout
 
-  # test actually purging a bundle
-  # for this we just need to set up a fake ANTIDOTE_HOME so we can purge it
-  ANTIDOTE_HOME=$BASEDIR/.tmp/tests/purge
-  [[ -d $ANTIDOTE_HOME ]] && rm -rf $ANTIDOTE_HOME
-
+  setup_fakezdotdir purge
   bundle="foo/bar"
   bundledir="$ANTIDOTE_HOME/https-COLON--SLASH--SLASH-github.com-SLASH-foo-SLASH-bar"
+  pluginsfile=${ZDOTDIR:-~}/.zsh_plugins.txt
+  expectedfile=$FAKEZDOTDIR/.zsh_plugins_after_purge.txt
 
-  # we don't need to test this, but it makes for a nice test output story
-  @test "purge setup: bundle directory does not exist" ! -d "$bundledir"
-  mkdir -p $bundledir
-  @test "purge setup: bundle directory exists" -d "$bundledir"
+  # we don't need to test this, but it helps tell the test story
+  @test "purge setup: bundle dir exists" -d "$bundledir"
 
-  # purge!
-  actual=$(antidote purge $bundle 2>&1)
-  exitcode=$?
-  @test "'antidote purge $bundle' existing bundle succeeds" $exitcode -eq 0
-  @test "'antidote purge $bundle' existing bundle correctly removed" ! -d "$bundledir"
+  actual=$(antidote purge foo/bar 2>&1); exitcode=$?
+  @test "'antidote purge foo/bar' succeeds" $exitcode -eq 0
+  @test "'antidote purge foo/bar' bundle dir removed" ! -d "$bundledir"
+
+  diffout=$(diff $pluginsfile $expectedfile); exitcode=$?
+  @test "'antidote purge foo/bar' file diff succeeds" $exitcode -eq 0
+  @test "'antidote purge foo/bar' commented out bundlefile correctly" -z "$diffout"
+
+  # teardown
+  ZDOTDIR=$OLD_ZDOTDIR
 }
 
 # antidote purge --all
@@ -58,14 +62,13 @@ source $BASEDIR/antidote.zsh
   local actual expected exitcode bundle bundledir
 
   # to test purging all bundles, we've got to make a full fake zdotdir
-  setup_fakezdotdir purge2
+  setup_fakezdotdir purge_all
   pluginsfile=${ZDOTDIR:-~}/.zsh_plugins.txt
 
   zstyle ':antidote:purge:all' answer 'n'
   actual=$(antidote purge --all 2>&1)
   exitcode=$?
   @test "'antidote purge --all' with answer=no fails" $exitcode -ne 0
-
 
   bakfiles=($ZDOTDIR/.zsh_plugins.*.bak(N))
   @test "No backup zsh_plugins file exists" $#bakfiles -eq 0
