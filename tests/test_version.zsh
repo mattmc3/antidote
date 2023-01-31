7bca66a0488e37fb3fc33d212b88a5ddbcf9771b
mattmc3 <mattmc3@gmail.com>
2023-01-31 08:03:54 -0500

    Bump version to 1.7.2

--- tests/test_version.zsh
+++ tests/test_version.zsh
@@ -20,7 +20,7 @@ source $BASEDIR/antidote.zsh
 () {
   local expected actual gitsha
   gitsha=$(git -C "$BASEDIR" rev-parse --short HEAD 2>/dev/null)
-  expected="antidote version 1.7.1 ($gitsha)"
+  expected="antidote version 1.7.2 ($gitsha)"
   actual="$(antidote -v 2>&1)"
   @test "'antidote -v' prints '$expected'" $expected = $actual
   @test "'-v' and '--version' print identical outputs" "$actual" = "$(antidote --version 2>&1)"
