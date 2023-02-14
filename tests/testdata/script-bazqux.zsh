57ae051e9971aab7ce99bc7a960328b77869f097
mattmc3 <mattmc3@gmail.com>
2023-02-14 14:58:02 -0500

    Add testdata

---
+++ tests/testdata/script-bazqux.zsh
@@ -0,0 +1,2 @@
+fpath+=( $ANTIDOTE_HOME/git-AT-github.com-COLON-baz-SLASH-qux )
+source $ANTIDOTE_HOME/git-AT-github.com-COLON-baz-SLASH-qux/qux.plugin.zsh
---
+++ tests/testdata/script-foobar.zsh
@@ -0,0 +1,2 @@
+fpath+=( $ANTIDOTE_HOME/https-COLON--SLASH--SLASH-github.com-SLASH-foo-SLASH-bar )
+source $ANTIDOTE_HOME/https-COLON--SLASH--SLASH-github.com-SLASH-foo-SLASH-bar/bar.plugin.zsh
