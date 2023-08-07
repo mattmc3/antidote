936a877ec42498b31c82d745ade2b2174cbad888
mattmc3 <mattmc3@gmail.com>
2023-08-07 08:51:29 -0400

    Fix #138
    - Add Zsh version check earlier in the load

--- antidote.zsh
+++ antidote.zsh
@@ -3,9 +3,15 @@ if test -z "$ZSH_VERSION"; then
   shellname=$(ps -p $$ -oargs= | awk 'NR=1{print $1}')
   echo >&2 "antidote: Expecting zsh. Found '$shellname'."
   return 1
-fi
+else
+  autoload -Uz is-at-least
+  if ! is-at-least 5.4.2; then
+    echo >&2 "antidote: Unsupported Zsh version '$ZSH_VERSION'. Expecting Zsh >5.4.2."
+    return 1
+  fi
 
-typeset -f __antidote_setup &>/dev/null && unfunction __antidote_setup
-0=${(%):-%N}
-autoload -Uz ${0:A:h}/functions/__antidote_setup
-__antidote_setup
+  typeset -f __antidote_setup &>/dev/null && unfunction __antidote_setup
+  0=${(%):-%N}
+  autoload -Uz ${0:A:h}/functions/__antidote_setup
+  __antidote_setup
+fi
