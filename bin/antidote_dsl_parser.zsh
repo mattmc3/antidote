02d530cdaf8b609c20aac2ec2a6c3b312c1843c2
mattmc3 <mattmc3@gmail.com>
2026-01-13 17:31:40 -0500

    Add DSL parser

---
+++ bin/antidote_dsl_parser.zsh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+
+# ./bin/antidote_dsl_parser.zsh <./tests/tmp_home/.zsh/.zsh_plugins.txt
+
+TAB="$(printf '\t')"
+
+parse_antidote() {
+  local line lineno arg argno
+  local -a args
+
+  lineno=1
+  while IFS= read -r line; do
+    # (z): use shell wordsplitting rules
+    args=(${(z)line})
+    argno=1
+    for arg in $args; do
+      [[ $arg == \#* ]] && break
+      if (( argno == 1 )); then
+        printf '__lineno__:%s%s__bundle__:%s' "$lineno" "$TAB" "$arg"
+      else
+        printf '%s%s' "$TAB" "$arg"
+      fi
+      (( argno++ ))
+    done
+    [[ $argno -gt 1 ]] && printf '\n'
+    (( lineno++ ))
+  done
+}
+parse_antidote "$@"
