efdc5afa7512776bd66002d35e16880e283929c4
mattmc3 <mattmc3@gmail.com>
2026-01-13 21:36:38 -0500

    Add bundle processing example

--- bin/antidote-bundle.sh
+++ bin/antidote-bundle.sh
@@ -1,37 +1,55 @@
 #!/usr/bin/env dash
 # shellcheck disable=SC3043
 
-antidote_script() {
-  local lineno="$1"
-  shift
-  printf '%s script: %s\n' "$lineno" "$@"
+THIS_SCRIPT="$(cd "$(dirname "$0")" && pwd)/$(basename "$0")"
+PARSER_SCRIPT="$(dirname "$THIS_SCRIPT")"/antidote_dsl_parser.zsh
+TAB="$(printf '\t')"
+
+ensure_cloned() {
+  local arg
+  echo "Ensuring cloned... $*"
+  for arg; do
+    echo "   arg: $arg"
+  done
+}
+
+emit_zsh_script() {
+  local arg
+  echo "Emmitting Zsh script... $*"
+  for arg; do
+    echo "   arg: $arg"
+  done
+}
+
+parse_dsl() {
+  zsh "$PARSER_SCRIPT"
+}
+
+# Usage: bundle_apply <command> < input.tsv
+# For each TSV line, splits fields into "$@" and calls: command "$@"
+bundle_apply() {
+  cmd=$1
+  while IFS= read -r line; do
+    # shellcheck disable=SC2086
+    IFS=$TAB set -- $line
+    "$cmd" "$@"
+  done
 }
 
 antidote_bundle() {
-  local bundle line lineno
-  if [ $# -gt 0 ]; then
-    lineno=0
-    for bundle in "$@"; do
-      printf '%b' "$bundle" | tr -d "\r" | while IFS= read -r line || [ -n "$line" ]; do
-        # printf 'Bundle %s\n' "$line"
-        lineno=$((lineno + 1))
-        antidote_script "$lineno" "$line"
-      done
-    done
-  elif [ ! -t 0 ]; then
-    lineno=0
-    while IFS= read -r line || [ -n "$line" ]; do
-      lineno=$((lineno + 1))
-      antidote_script "$lineno" "$line"
-    done
-  fi
+  local bundles
 
-  # elif [[ ! -t 0 ]]; then
-  #   local data
-  #   while IFS= read -r data || [[ -n "$data" ]]; do
-  #     input+=("$data")
-  #   done
-  # fi
-  # printf '%s\n' "${input[@]}"
+  bundles="$({
+    [ $# -eq 0 ] || printf '%b\n' "$*"
+    [ -t 0 ] || cat
+  })"
+
+  printf '%s\n' "$bundles" |
+    parse_dsl |
+    bundle_apply ensure_cloned
+
+  printf '%s\n' "$bundles" |
+    parse_dsl |
+    bundle_apply emit_zsh_script
 }
 antidote_bundle "$@"
