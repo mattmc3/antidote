9bb69ab99c6f05d6e6ae237f7ce222eeeb5b4a14
mattmc3 <mattmc3@gmail.com>
2026-08-07 13:20:44 -0400

    Bump version to 2.3.0

--- antidote.zsh
+++ antidote.zsh
@@ -2418,7 +2418,7 @@ antidote() {
 # Initialize antidote global variables from zstyles and environment.
 {
   typeset -g ANTIDOTE_ZSH="${0:a}"
-  typeset -g _ANTIDOTE_VERSION="2.2.2"
+  typeset -g _ANTIDOTE_VERSION="2.3.0"
   typeset -g ANTIDOTE_TMPDIR=${ANTIDOTE_TMPDIR:-$TMPDIR}
 
   typeset -g _ANTIDOTE_GIT_SITE _ANTIDOTE_GIT_PROTOCOL _ANTIDOTE_GIT_CMD _ANTIDOTE_FZF_CMD _ANTIDOTE_PATH_STYLE
